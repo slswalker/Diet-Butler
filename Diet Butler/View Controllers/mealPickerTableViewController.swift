cf2cb7fdf81826ac2c716d2cb2e21270adadd52d
Sam Walker <slw1206@g.rit.edu>
2015-07-18 22:34:27 -0700

    Last commit for the night

---
+++ Diet Butler/View Controllers/mealPickerTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  mealPickerTableViewController.swift
+//  Diet Butler
+//
+//  Created by Samuel Walker on 7/18/15.
+//  Copyright © 2015 Samuel Walker. All rights reserved.
+//
+
+import UIKit
+
+class mealPickerTableViewController: UITableViewController {
+	
+}
